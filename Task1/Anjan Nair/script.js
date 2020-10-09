19eafbf62d333d9f44713c39f9084362cc29f38d
Anjan Nair <22571613+anjannair@users.noreply.github.com>
2020-10-09 17:44:27 +0530

    Added explanatory function names
    Removed bad regex

--- Task1/Anjan Nair/script.js
+++ Task1/Anjan Nair/script.js
@@ -1,4 +1,4 @@
-function username() {
+function username_check() {
     //to check username for letters
     var user = document.forms["inputer"]["username"].value;
     var letters = /^[A-Za-z]+$/;
@@ -8,7 +8,8 @@ function username() {
         alert("Your username must not contain special charaters or numbers. Only alphabets.");
     }
 }
-function phonenumber() {
+
+function number_check(){
     //to check phone number
     var phone = document.forms["inputer"]["phone"].value;
     var number = /^[6-9]\d{9}$/;
@@ -19,8 +20,8 @@ function phonenumber() {
     }
 }
 
-function emailcheck() {
-    //to check for valid email
+function email_check(){
+ //to check for valid email
     var email = document.forms["inputer"]["email"].value;
     var emailregex = /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
 
@@ -29,19 +30,28 @@ function emailcheck() {
     }
 }
 
-function githubcheck() {
-    //to check for valid link
+function githubprofile_check(){
+//to check for valid link
     var link = document.forms["inputer"]["git"].value;
-    var linkcheck = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g;
+    var linkcheck = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/;
 
     if(!link.match(linkcheck)){
         alert("Enter a valid link");
     }
     else{
         //checks for valid github profile link
-        var githubprofile = /(?:https?:)?\/\/(?:www\.)?github\.com\/(?P<login>[A-z0-9_-]+)\/?/;
-        if(!link.match(githubprofile)){
-            alert("Enter a valid github profile link!");
+        if(!link.startsWith("https://github.com"|| !link.startsWith("https://www.github.com"))){
+            alert("Enter a valid GitHub link!");
         }
     }
+}
+
+function password_match(){
+//to check for password match
+var password1 = document.forms["inputer"]["pwd1"].value;
+var password2 = document.forms["inputer"]["pwd2"].value;
+
+if(password1 != password2){
+    alert("Passwords don't match!");
+}
 }
